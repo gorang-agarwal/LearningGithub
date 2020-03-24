17ad2c2bb4d52374e4ce7eb8c08cc3bfd0f68fc4
Gorang Agarwal <gorang2001@gmail.com>
2020-03-24 13:05:13 +0530

    HelloWord modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
         public static void main(string[] args) {
-               system.out.printIn("Hello World");
+                System.out.println("Hello World");
+		System.out.println("This is just for modifying");
       }
 }
